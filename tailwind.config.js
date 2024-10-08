ff91f83640550b996d6908be77743fde26864283
Tay Ming Hui <mtay0031@student.monash.edu>
2024-10-08 19:35:24 +0800

    added timeline with scroll animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+      animation: {
+        'linear': 'moveline 6s linear forwards',
+      },
+      transition:{
+        'ease': '10s ease'
+      }
   },
   plugins: [],
 }
-
+}
--- w9.js
+++ w9.js
@@ -1,12 +1,12 @@
-// var vg_1 = "w9.json";
-// vegaEmbed("#map", vg_1).then(function(result){
-//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
+var vg_1 = "w9.json";
+vegaEmbed("#map", vg_1).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
-// var vg_2 = "w10.json";
-// vegaEmbed("#bar", vg_2).then(function(result){
-//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-// }).catch(console.error);
+var vg_2 = "revenue.json";
+vegaEmbed("#area", vg_2).then(function(result){
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
 // Create container elements
 document.addEventListener('DOMContentLoaded', function() {
